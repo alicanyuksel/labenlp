3c5a4949488512fde0576dd35879626100c7f507
Alican Yüksel <alicanyuksel@outlook.com>
2021-05-13 12:38:22 +0200

    Add sidebar component

---
+++ src/components/navigationBar.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="wrapper">
+        <nav id="sidebar">
+            <router-link to="/">Back to home</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+	components: {},
+	
+};
+</script>
+
+<style>
+
+.wrapper {
+    position: fixed;
+    left: 0;
+    top: 0;
+    background-color:gray;
+    width: 10%;
+    height: 100%;   
+}
+
+#sidebar {
+    margin-top: 30% ;
+    
+    
+}
+
+a {
+	text-decoration: none;
+	color: white;
+	text-align: center;
+	font-size: 20px;
+	border-radius: 8px;
+	cursor: pointer;
+}
+
+</style>
