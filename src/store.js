7714b4576df10aaaee1ded302f46aadce6be7d80
Alican Yüksel <alicanyuksel@outlook.com>
2021-05-21 00:58:51 +0200

    Add some state and mutation for the input classes

--- src/store.js
+++ src/store.js
@@ -7,24 +7,33 @@ export const store = new Vuex.Store({
     state: {
         inputText: "",
         inputReceived: false,
-       
+        allClasses: [],
+        anyClasseAdded: false,
     },
     mutations: {
         saveInputSentence(state, payload) {
             state.inputText = payload.value;
-            
         },
         isInputReceived(state, payload) {
             state.inputReceived = payload.value;
         },
         initializeInput(state) {
-            state.inputText = ""
-            state.inputReceived = false
-        }
+            state.inputText = "";
+            state.inputReceived = false;
+        },
+        addClasse(state, payload) {
+            if (payload.value != "") {
+                state.allClasses.push(payload.value);
+                state.anyClasseAdded = true;
+            }
+        },
     },
     getters: {
         getInputText(state) {
             return state.inputText;
-        }
-    }
+        },
+        getAllClasses(state) {
+            return state.allClasses;
+        },
+    },
 });
