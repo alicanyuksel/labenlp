79b9111e2868262c902ffa722b1cee2b246349eb
Alican Yüksel <alicanyuksel@outlook.com>
2021-05-21 03:46:57 +0200

    Add a class block component

---
+++ src/components/classBlock.vue
@@ -0,0 +1,31 @@
+<template>
+	<span class="color-box" :style="{ backgroundColor: bgColor }">
+		{{ className }}
+	</span>
+</template>
+
+<script>
+export default {
+	name: "classBlock",
+	props: {
+		className: String,
+		bgColor: String,
+	},
+};
+</script>
+
+<style>
+.color-box {
+	border-radius: 5px;
+	padding: 5px;
+	font-weight: bold;
+	color: black;
+	width: 1rem;
+	height: 1rem;
+	margin-right: 1rem;
+}
+
+.color-box:active {
+	background-color: red;
+}
+</style>
