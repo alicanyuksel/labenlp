2f2c8c76bfcc77c1d792eeca19bae3b4bb24f9a8
Alican Yüksel <alicanyuksel@outlook.com>
2021-06-04 16:10:18 +0200

    Add a token component

---
+++ src/components/tokenBlock.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<span
+			v-for="token in allTokenNames"
+			:key="token.index"
+		>
+			{{ token }}
+		</span>
+	</div>
+</template>
+
+<script>
+import { mapState } from "vuex"
+ 
+export default {
+	name: "tokenBlock",
+	props: {
+
+	},
+	computed: {
+		...mapState(["allTokenDetails", "allTokenNames"])
+	}
+};
+</script>
+
+<style>
+</style>
