b13ac27643df7a86ae45851c26ed7b901ff9585c
Alican Yüksel <alicanyuksel@outlook.com>
2021-07-25 18:22:31 +0200

    Add the Q&A store in index

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vuex from "vuex";
 import Vue from 'vue'
 import { nerStore } from "./modules/ner.js"
+import { qaStore } from "./modules/qa.js"
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     modules: {
     ner: nerStore,
+    qa: qaStore
     }
   });
   
