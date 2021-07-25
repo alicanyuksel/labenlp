9f6323a1fbc6d71d7c8090f176c4fecb2f15deaa
Alican Yüksel <alicanyuksel@outlook.com>
2021-07-25 21:13:06 +0200

    Clean up code

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
-import nerAnnotation from './components/ner/nerAnnotation.vue'
-import questionAnswerAnnotation from './components/qa/questionAnswerAnnotation.vue'
-import homePage from './components/homePage.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import nerAnnotation from "./components/ner/nerAnnotation.vue";
+import questionAnswerAnnotation from "./components/qa/questionAnswerAnnotation.vue";
+import homePage from "./components/homePage.vue";
 
 import { store } from "./store";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
     { path: "/", component: homePage },
     { path: "/ner", component: nerAnnotation },
-    { path: "/question-answer", component: questionAnswerAnnotation }
-  ]
+    { path: "/question-answer", component: questionAnswerAnnotation },
+];
 
 const router = new VueRouter({
-  mode: "history",
-  routes
-})
+    mode: "history",
+    routes,
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
