f34ad33c0979e8e1870817c05524f562a024f08b
Alican Yüksel <alicanyuksel@outlook.com>
2021-07-25 18:20:39 +0200

    Add a loadfile component for Q&A models

---
+++ src/components/qa/qaLoadFile.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="file-upload">
+		<input
+			class="file-input"
+			type="file"
+			name="textfile"
+			accept=".txt"
+			@change="readFile"
+		/>
+	</div>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+
+export default {
+	name: "loadTextFile",
+	methods: {
+		...mapMutations("qa",["saveInput", "setInputReceived"]),
+		readFile(e) {
+			let files = e.target.files;
+			if (!files.length) return;
+			let reader = new FileReader();
+			reader.addEventListener("load", (event) => {
+				this.saveInput({
+					value: event.target.result
+				});
+				this.setInputReceived({
+					value: true
+				});
+			});
+			reader.readAsText(files[0]);
+		},
+	},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.file-upload {
+	position: relative;
+}
+
+input.file-input {
+	text-decoration: none;
+	background-color: white;
+	color: black;
+	padding: 15px 32px;
+	text-align: center;
+	font-size: 15px;
+	border-radius: 8px;
+	border-style: dashed;
+	border-color: rgb(177, 114, 20);
+	cursor: pointer;
+}
+
+h3 {
+	margin: 40px 0 0;
+}
+</style>
