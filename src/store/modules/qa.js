615fc7f8053ba950104d1ca0deb209277f5f936b
Alican Yüksel <alicanyuksel@outlook.com>
2021-08-04 16:27:50 +0200

    Add an export annotation function for Q&A annotations

--- src/store/modules/qa.js
+++ src/store/modules/qa.js
@@ -40,6 +40,26 @@ export const qaStore = {
         resetAll(state) {
             Object.assign(state, getDefaultState())            
         },
+        exportAnnotation(state) {
+            const filename = "qa_annotations.json";
+
+            let output = {
+                context: state.inputText,
+                qas: state.allAnnoationDetails,
+            };
+            
+            const jsonOutput = JSON.stringify(output, null, 4);
+            let element = document.createElement("a");
+            element.setAttribute(
+                "href",
+                "data:text/plain;charset=utf-8," + encodeURIComponent(jsonOutput)
+            );
+            element.setAttribute("download", filename);
+            element.style.display = "none";
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
+        }
     },
     getters: {
         isInputReceived(state) {
